d8ca886c1adbce91f1336d6641165fb56cd2d66a
therealmbittarelli <maggiekmcclellan@gmail.com>
2020-02-02 14:05:33 -0500

    changing spaced-repetition to spaced_repetition

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL
-    || 'postgresql://dunder_mifflin@localhost/spaced-repetition',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/spaced-repetition-test',
+    || 'postgresql://dunder_mifflin@localhost/spaced_repetition',
+  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/spaced_repetition_test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };
