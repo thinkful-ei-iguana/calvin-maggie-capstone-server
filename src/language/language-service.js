3d093b6bde38f5a2e2e2be3ffa634ed52bdf8e90
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2020-02-04 08:10:26 -0700

    head and guess api routing points filled out

--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,33 +1,33 @@
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
-      .from('language')
+      .from("language")
       .select(
-        'language.id',
-        'language.name',
-        'language.user_id',
-        'language.head',
-        'language.total_score',
+        "language.id",
+        "language.name",
+        "language.user_id",
+        "language.head",
+        "language.total_score"
       )
-      .where('language.user_id', user_id)
-      .first()
+      .where("language.user_id", user_id)
+      .first();
   },
 
   getLanguageWords(db, language_id) {
     return db
-      .from('word')
+      .from("word")
       .select(
-        'id',
-        'language_id',
-        'original',
-        'translation',
-        'next',
-        'memory_value',
-        'correct_count',
-        'incorrect_count',
+        "id",
+        "language_id",
+        "original",
+        "translation",
+        "next",
+        "memory_value",
+        "correct_count",
+        "incorrect_count"
       )
-      .where({ language_id })
-  },
-}
+      .where({ language_id });
+  }
+};
 
-module.exports = LanguageService
+module.exports = LanguageService;
